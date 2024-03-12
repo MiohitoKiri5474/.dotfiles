33a22d5ea0622d406f6c3378d94735fa6e260556
MiohitoKiri5474 <lltzpp@gmail.com>
2024-03-12 16:43:56 +0800

    Remove ensure_install in lsp.lua
    Remove ensure_install in lsp.lua, using LazyVim default setup instead (with LazyExtra).

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,9 +3,10 @@ return {
 	optional = true,
 	opts = {
 		formatters_by_ft = {
-			["c"] = { "clang_format" },
-			["h"] = { "clang_format" },
-			["lua"] = { "stylua" },
+			c = { "clang_format" },
+			h = { "clang_format" },
+			lua = { "stylua" },
+			go = { "goimports", "gofumpt" },
 		},
 	},
 	keys = {
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,33 +1,5 @@
 return {
 	{
-		"williamboman/mason.nvim",
-
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
-
-			mason.setup({
-				ui = {
-					icons = {
-						server_installed = "✓",
-						server_pending = "➜",
-						server_uninstalled = "✗",
-					},
-				},
-			})
-			mason_lspconfig.setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "gopls" },
-				automatic_installation = true,
-			})
-		end,
-		opts = function(_, opts)
-			table.insert(opts.ensure_installed, { "black", "clang-format", "isort" })
-		end,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
