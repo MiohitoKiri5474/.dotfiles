a8975c41ba56ebc002685e5eef93b9c13d2b8650
MiohitoKiri5474 <lltzpp@gmail.com>
2024-03-16 21:26:22 +0800

    Update nvim-silicon background color

--- nvim/.config/nvim/lua/plugins/nvim-silicon.lua
+++ nvim/.config/nvim/lua/plugins/nvim-silicon.lua
@@ -8,6 +8,7 @@ return {
 			font = "SauceCodePro Nerd Font=34;SauceCOdePro Nerd Font=34",
 			to_clipboard = true,
 			theme = "Solarized (dark)",
+			background = "#0d90b2",
 			output = function()
 				return "~/Desktop/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
 			end,
