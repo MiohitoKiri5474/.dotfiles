d217a12556893cbed960ba8d2ff72363f0719e3e
MiohitoKiri5474 <lltzpp@gmail.com>
2024-04-15 12:05:40 +0800

    Add Chinese input source auto switch
    By using
    [laishulu/vim-macos-ime](https://github.com/laishulu/vim-macos-ime) to
    auto switch input source.

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -198,4 +198,11 @@ return {
 		cmd = "IncRenamd",
 		config = true,
 	},
+	{
+		"laishulu/vim-macos-ime",
+		config = function()
+			vim.g.macosime_normal_ime = "com.apple.keylayout.ABC"
+			vim.g.macosime_cjk_ime = "com.apple.inputmethod.TCIM.Zhuyin"
+		end,
+	},
 }
