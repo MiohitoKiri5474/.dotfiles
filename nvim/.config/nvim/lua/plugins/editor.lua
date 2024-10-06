38046515089d4ce9cd1acf6b19a04e8feae66f2a
MiohitoKiri5474 <lltzpp@gmail.com>
2024-10-06 21:52:37 +0800

    Add inc-rename config

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -149,7 +149,19 @@ return {
 	},
 	{
 		"smjonas/inc-rename.nvim",
-		cmd = "IncRenamd",
+		cmd = "IncRename",
+		keys = {
+			{
+				"<leader>rn",
+				function()
+					return ":IncRename " .. vim.fn.expand("<cword>")
+				end,
+				desc = "Incremental rename",
+				mode = "n",
+				noremap = true,
+				expr = true,
+			},
+		},
 		config = true,
 	},
 	{
