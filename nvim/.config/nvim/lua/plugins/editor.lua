86873705d179b0676b26b1b9fa6d0515618bf62d
MiohitoKiri5474 <lltzpp@gmail.com>
2024-10-31 23:09:48 +0800

    Neovim yazi support

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -114,6 +114,32 @@ return {
 			require("telescope").load_extension("file_browser")
 		end,
 	},
+	---@type LazySpec
+	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>yy",
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+		},
+		---@type YaziConfig
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = false,
+			keymaps = {
+				show_help = "<f1>",
+			},
+		},
+	},
 	{
 		"mizlan/delimited.nvim",
 		keys = {
