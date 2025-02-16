08eb021022ceae3624981cbaacddcbd75ba946b9
MiohitoKiri5474 <lltzpp@gmail.com>
2025-02-16 09:59:47 +0800

    NeoVim: Upgrade LazyVim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,34 +1,39 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc", "py" },
-	callback = function()
-		vim.wo.spell = false
-		vim.wo.concealleave = 0
-	end,
+  pattern = { "json", "jsonc", "py" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.concealleave = 0
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "c", "cpp" },
-	callback = function()
-		vim.g.autoformat = true
-		vim.opt_local.expandtab = true
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-		vim.opt_local.softtabstop = 4
-	end,
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.g.autoformat = true
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "cpp" },
-	callback = function()
-		vim.b.autoformat = false
-	end,
+  pattern = { "cpp" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
 })
