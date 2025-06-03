ff4386ab394ae6191fe5ebf2216c008b69e28bff
MiohitoKiri5474 <lltzpp@gmail.com>
2025-06-03 17:52:23 +0800

    NeoVim: Add CopilotChat, rearrange LazyExtras and enable Copilot with Extras

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -81,6 +81,17 @@ return {
           return vim.b.trouble_lualine ~= false and symbols.has()
         end,
       })
+      table.insert(
+        opts.sections.lualine_x,
+        1,
+        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
     end
 
     return opts
