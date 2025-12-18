799bdb7649272b4e4aeeb9b17591a60f97209883
MiohitoKiri5474 <lltzpp@gmail.com>
2025-12-18 21:33:34 +0800

    NeoVim: Update vimtex default previewer

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -1,25 +1,22 @@
 return {
-  "lervag/vimtex",
-  lazy = false, -- Load VimTeX on startup
-  config = function()
-    vim.g.vimtex_view_method = "skim"
-    vim.g.vimtex_compiler_method = "latexmk"
-    vim.g.vimtex_view_general_viewer = "/Applications/Skim.app/Contents/MacOS/Skim"
-    vim.g.vimtex_view_general_options = "--reuse-instance"
-    vim.g.vimtex_compiler_latexmk = {
-      options = {
-        "-pdf",
-        "-shell-escape",
-        "-interaction=nonstopmode",
-        "-synctex=1",
+  {
+    "lervag/vimtex",
+    lazy = false, -- Load VimTeX on startup
+    config = function()
+      vim.g.vimtex_view_general_viewer = "zathura"
+      vim.g.vimtex_view_zathura_options = "-reuse-instance"
+      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_view_synctex = true
+      vim.g.vimtex_compiler_method = "latexmk"
+    end,
+    keys = {
+      {
+        "<leader>vt",
+        ":VimtexCompile<Return>",
       },
-    }
-    vim.g.vimtex_format_enabled = 1
-  end,
-  keys = {
-    {
-      "<leader>vt",
-      ":VimtexCompile<Return>",
     },
+  },
+  {
+    "3rd/image.nvim",
   },
 }
