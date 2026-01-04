91e6464b3860cece54336d025d0299a54d7b8a4e
MiohitoKiri5474 <lltzpp@gmail.com>
2026-01-04 16:14:57 +0800

    NeoVim: Update cpp template

--- nvim/.config/nvim/lua/templates/cpp.lua
+++ nvim/.config/nvim/lua/templates/cpp.lua
@@ -5,14 +5,14 @@ local function base_template(relative_path, filename)
 // by. MiohitoKiri5474
 #include <bits/stdc++.h>
 
-#pragma GCC optimize ( "O3" )
-#pragma loop_opt ( on )
+#pragma GCC optimize ("O3")
+#pragma loop_opt (on)
 
 using namespace std;
 
 // type and pair definition
 using LL = long long;
-using pii = pair < LL, LL >;
+using pii = pair<LL, LL>;
 using sstr = stringstream;
 #define F first
 #define S second
@@ -21,20 +21,20 @@ using sstr = stringstream;
 // general definition
 #define pb push_back
 #define ep emplace_back
-#define GL(n) getline ( cin, n )
+#define GL(n) getline(cin, n)
 #define gura ios::sync_with_stdio(false);cin.tie(0)
 
 // define heap
-template < class T > using MaxHeap = priority_queue < T, vector < T >, less < T > >;
-template < class T > using MinHeap = priority_queue < T, vector < T >, greater < T > >;
+template <class T> using MaxHeap = priority_queue<T, vector<T>, less<T>>;
+template <class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 
 // number definition
 #define INF 0x3f3f3f3f
 
 // some defiine for programing contest
 #define int LL
-inline void print_ans ( bool flag ) {
-    cout << ( flag ? "Yes" : "No" ) << endl;
+inline void print_ans (bool flag) {
+    cout << (flag ? "Yes" : "No") << endl;
 }
 const int maxN = 100005;
 
